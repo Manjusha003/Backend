e7c44beda3b05d7601a768e20ec900ff237d051c
Manjusha003 <manjusharaut003@gmail.com>
2022-08-29 14:28:52 +0530

    middleware-2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://ManjushaRaut:OjllbBZtnJwKxD3I@cluster0.3qd4bit.mongodb.net/Middleware?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
