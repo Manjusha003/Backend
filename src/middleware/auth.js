d10f47dc6ccc7f95349e05fe571291b27f5844fa
Manjusha003 <manjusharaut003@gmail.com>
2022-08-31 15:12:33 +0530

    try-catch

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,38 @@
-const authenticate = function(req, req, next) {
-    //check the token in request header
-    //validate this token
+const jwt=require("jsonwebtoken")
+
+const authenticate =  function (req, res, next) {
+
+    let token = req.headers["x-auth-token"];
+   if (!token) token = req.headers["x-auth-token"];
+
+
+    if (!token) return res.send({ status: false, msg: "token must be present" });
+
+    console.log(token);
+
+    let decodedToken = jwt.verify(token, "functionup-Plutonium");
+    if (!decodedToken)
+        return res.send({ status: false, msg: "token is invalid" });
+
+    req.loggedInUser = decodedToken.userId;
 
     next()
 }
 
 
-const authorise = function(req, res, next) {
+const authorise =   function (req, res, next) {
     // comapre the logged in user's id and the id in request
+   
+    let userToBeModified = req.params.userId
+
+   
+
+    if (userToBeModified !== req.loggedInUser)
+        return res.send({ status: false, msg: 'User logged is not allowed to modify the requested users data' })
+
+
     next()
 }
+
+module.exports.authenticate=authenticate;
+ module.exports.authorise=authorise
